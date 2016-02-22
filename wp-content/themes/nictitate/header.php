7ec8c713052cd01fee2e48c34aef239cb489c3a6
Maurice Moret <maurice@burst-digital.com>
2016-02-22 21:31:02 +0100

    Added Chimpmate plugin and removed the ugly popup

--- wp-content/themes/nictitate/header.php
+++ wp-content/themes/nictitate/header.php
@@ -47,8 +47,8 @@
 
     </script>
 
-    <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us12.list-manage.com","uuid":"30c907614f20684cf6af1a82b","lid":"9a25b032ba"}) })</script>
-
+    <!--<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us12.list-manage.com","uuid":"30c907614f20684cf6af1a82b","lid":"9a25b032ba"}) })</script>
+-->
         <div class="kopa-wrapper">
 
             <div class="kopa-background"></div>
